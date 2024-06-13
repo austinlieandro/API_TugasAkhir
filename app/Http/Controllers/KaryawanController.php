5dd83e8fbacaf3eb0430baa735858761c4408c0f
Austin Lieandro <72506433+austinlieandro@users.noreply.github.com>
2024-06-13 15:17:33 +0700

    UPDATE

--- app/Http/Controllers/KaryawanController.php
+++ app/Http/Controllers/KaryawanController.php
@@ -8,8 +8,9 @@ use Illuminate\Support\Facades\Validator;
 
 class KaryawanController extends Controller
 {
-    public function karyawan(request $request){
-        $validator = Validator::make($request->all(),[
+    public function karyawan(request $request)
+    {
+        $validator = Validator::make($request->all(), [
             'nama_karyawan' => 'required',
             'bengkels_id' => 'required',
         ]);
@@ -23,7 +24,7 @@ class KaryawanController extends Controller
             'bengkels_id' => (int)$request->bengkels_id,
         ]);
 
-        if($karyawan){
+        if ($karyawan) {
             return response()->json([
                 'status' => true,
                 'message' => 'Berhasil mendaftarkan karyawan',
@@ -36,7 +37,8 @@ class KaryawanController extends Controller
             'message' => 'Gagal mendaftarkan karyawan',
         ], 409);
     }
-    public function daftarKaryawan($id){
+    public function daftarKaryawan($id)
+    {
         $karyawan = karyawan::where('bengkels_id', $id)->get();
 
         if (!$karyawan) {
@@ -51,5 +53,63 @@ class KaryawanController extends Controller
             'message' => 'karyawan ditemukan',
             'bengkel' => $karyawan,
         ], 201);
+    }
+
+    public function updateKaryawan(request $request, $bengkels_id, $karyawan_id)
+    {
+        $validator = Validator::make($request->all(), [
+            'nama_karyawan' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        $karyawan = Karyawan::where('id', $karyawan_id)->where('bengkels_id', $bengkels_id)->first();
+        if (!$karyawan) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Gagal memperbarui data karyawan'
+            ], 404);
+        }
+
+        $karyawan->nama_karyawan = $request->nama_karyawan;
+        $karyawan->save();
+
+        return response()->json([
+            'status' => true,
+            'message' => 'Berhasil memperbarui data karyawan',
+            'karyawan' => $karyawan,
+        ], 200);
+    }
+
+    public function deleteKaryawan(request $request, $bengkels_id, $karyawan_id)
+    {
+        $validator = Validator::make($request->all(), []);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        $karyawan = Karyawan::where('id', $karyawan_id)->where('bengkels_id', $bengkels_id)->first();
+
+        if (!$karyawan) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Karyawan tidak ditemukan',
+            ], 404);
+        }
+
+        if ($karyawan->delete()) {
+            return response()->json([
+                'status' => true,
+                'message' => 'Berhasil menghapus data karyawan',
+            ], 200);
+        } else {
+            return response()->json([
+                'status' => false,
+                'message' => 'Gagal menghapus data karyawan',
+            ], 500);
+        }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -16,6 +16,8 @@ Route::post('/daftarKaryawan', [KaryawanController::class, 'karyawan']);
 Route::post('/kendaraan/{userId}/{id}', [KendaraanController::class, 'updateKendaraan']);
 Route::post('/jamOperasional', [JamOperasionalController::class, 'inputJam']);
 Route::post('/updateOperasional/{bengkelId}/{id}', [JamOperasionalController::class, 'updateJamOperasional']);
+Route::post('/updateKaryawan/{bengkelId}/{id}', [KaryawanController::class, 'updateKaryawan']);
+Route::post('/deleteKaryawan/{bengkelId}/{id}', [KaryawanController::class, 'deleteKaryawan']);
 
 Route::get('/bengkel', [BengkelController::class, 'showAllbengkels']);
 Route::get('/bengkel/{id}', [BengkelController::class, 'detailBengkels']);
